200130739912ac286b82416b4d61074ad3bf3a01
wang <346059047@qq.com>
2020-02-09 21:58:40 +0800

    1

--- Test/src/Test/test1.java
+++ Test/src/Test/test1.java
@@ -3,5 +3,7 @@ package Test;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println("test.main()");
+		
+		///1111111111111111
 	}
 }
---
+++ Test/src/Test/test3.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class test3 {
+	public static void main(String[] args) {
+		
+	}
+}
